e5770898104aa925a1f1fb480ad5147ea291482b
kdm-korea <kdm_korea@naver.com>
2019-03-10 00:54:57 +0900

    [#1] Feat: Adapter Pattern
    인터페이스의 설계의 문제나 다른 인터페이스와의 상호 연결이 필요한 경우 사용하면 좋을 듯 하다.

---
+++ I_Need_Make/I_Need_Make/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_Need_Make
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            TryCatchMethod();
+        }
+
+        private static void TryCatchMethod() {
+            try {
+                FstTryMethod();
+            }
+            catch (IndexOutOfRangeException) {
+
+                throw;
+            }
+        }
+
+        private static void FstTryMethod(){
+            int a;
+            int b;
+            throw new IndexOutOfRangeException();
+        }
+
+    }
+
+    class MapEx : IMapExperiment<Program>
+    {
+
+    }
+
+    interface Duck
+    {
+        void quack();
+        void fly();
+    }
+
+    public class MallardDuck : Duck
+    {
+        public void fly() {
+            Console.WriteLine("Quack");
+        }
+
+        public void quack() {
+            Console.WriteLine("Im flying");
+        }
+    }
+
+    interface Turkey
+    {
+        void Gobble();
+        void fly();
+    }
+
+    public class WildTurkey : Turkey
+    {
+        public void fly() {
+            Console.WriteLine("Gobble gobble");
+        }
+
+        public void Gobble() {
+            Console.WriteLine("I'm flying a short distance");
+        }
+    }
+
+    class TurkeyAdapter : Duck
+    {
+        Turkey turkey;
+
+        public TurkeyAdapter(Turkey turkey) {
+            this.turkey = turkey;
+        }
+
+        public void fly() {
+            turkey.fly();
+        }
+
+        public void quack() {
+            turkey.Gobble();
+        }
+    }
+
+    class DuckTestDrive
+    {
+        public void ExMain() {
+            MallardDuck mallardDuck = new MallardDuck(); 
+            WildTurkey turkey = new WildTurkey();
+            TurkeyAdapter turkeyAdapter = new TurkeyAdapter(turkey);
+
+            TurkeyTalk(turkeyAdapter);
+
+            MallardTalk(mallardDuck);
+            MallardTalk(turkeyAdapter);
+
+            MallardDuck duck = new MallardDuck();
+        }
+
+        private void TurkeyTalk(TurkeyAdapter turkeyAdapter) {
+            turkeyAdapter.fly();
+            turkeyAdapter.quack();
+        }
+
+        private void MallardTalk(Duck duck) {
+            duck.fly();
+        }
+    }
+}
