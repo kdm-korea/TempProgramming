e988f9280a3cf32dc6741bef683b1486ec113bae
kdm-korea <kdm_korea@naver.com>
2019-03-11 16:46:38 +0900

    [#2] Feat: Using Azure database service
    아쥬라의 데이터베이스 사용

---
+++ DotNetConnectAzure/DotNetConnectAzure/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetConnectAzure
+{
+    class Program
+    {
+        public static void Main(string[] args) {
+            try {
+                AzureDBService();
+            }
+            catch (SqlException e) {
+                Console.WriteLine($"SqlException : {e}");
+                Console.ReadLine();
+            }
+            catch (Exception e) {
+                Console.WriteLine($"Exception : {e}");
+                Console.ReadLine();
+            }
+        }
+
+        private static void AzureDBService() {
+            using (SqlConnection connection = new SqlConnection(BankDatabase())) {
+                connection.Open();
+                string sql = GetColorName();
+
+                using (SqlCommand command = new SqlCommand(sql, connection)) {
+                    using (SqlDataReader reader = command.ExecuteReader()) {
+                        while (reader.Read()) {
+                            Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                        }
+                    }
+                }
+            }
+            throw new Exception();
+        }
+
+        private static string BankDatabase() {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "dongmindbserver.database.windows.net";
+            builder.UserID = "kdm";
+            builder.Password = "rlaehdals1!";
+            builder.InitialCatalog = "BankDatabase";
+            return builder.ConnectionString;
+        }
+
+        private static string GetColorName() {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName ");
+            sb.Append("FROM [SalesLT].[ProductCategory] pc ");
+            sb.Append("JOIN [SalesLT].[Product] p ");
+            sb.Append("ON pc.productcategoryid = p.productcategoryid;");
+            return sb.ToString();
+        }
+    }
+}
