2ba41a201facdd87cb89b0070b247f9acc6700a8
kdm-korea <kdm_korea@naver.com>
2019-03-18 17:48:56 +0900

    [#2] Feat: Programmers Algorithm

---
+++ Sorting/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            int[] num = new int[] { 123, 10, 2 };
+            //Console.WriteLine(num.Length);  //3
+            solution(num);
+        }
+
+        private static string solution(int[] numbers) {
+            List<char> array = new List<char>();
+            string answer = "";
+
+            for (int idx = 0; idx < numbers.Length; idx++) {
+                for (int i = 0; i < numbers[idx].ToString().Length; i++) {
+                    array.Add(numbers[idx].ToString()[i]);
+                }
+            }
+
+            array.Sort();
+            array.Reverse();
+
+            for (int idx = 0; idx < array.Count; idx++) {
+                answer += array[idx];
+            }
+            Console.WriteLine(answer);
+            return answer;
+        }
+    }
+}
