19d1d3100852ae122ff1e1e58fb29db547166be4
KDM_korea <kdm_korea@naver.com>
2019-05-31 12:16:38 +0900

    Create: Programmers algorithm lessins
    There are 𝑛 cargo trucks in a line to cross a one lane wide bridge over a river. The order of trucks cannot be
    changed and the weight of each truck may not be the same. Only 𝑤 trucks can be on the bridge at the same
    time. We assume that the length of the bridge is 𝑤 unit distance and each truck moves a unit distance in a unit
    time. The sum of the weights of the trucks on the bridge should be less than or equal to 𝐿 due to the maximum
    safe load of the bridge. Note that the weights of trucks that aren’t fully present on the bridge are not
    considered in the load calculation of the trucks on the bridge.

---
+++ Trucks/Trucks/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Trucks
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Solution result = new Solution();
+            int bridege_length = 2;
+            int weight = 10;
+            int[] truck_weights = new int[] { 7, 4, 5, 6 };
+
+            Console.WriteLine(result.solution(bridege_length, weight, truck_weights));
+        }
+    }
+}
---
+++ Trucks/Trucks/Solution.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Trucks
+{
+    class Solution
+    {
+        Queue<Truck> truck = new Queue<Truck>();
+        int idx = 0, time_stamp = 0, time = 0;
+        int total_weight = 0;
+
+        public int solution(int bridge_length, int weight, int[] truck_weights) {
+            while (true) {
+                if (total_weight <= weight && idx + 1 == truck_weights.Length) {
+                    truck.Enqueue(new Truck(truck_weights[idx++], 0));
+                    AddTimeStamp();
+                    OutofBridge(weight);
+                }
+                else {
+                    AddTimeStamp();
+                    OutofBridge(weight);
+                }
+
+                time++;
+
+                if (truck.Count == 0) {
+                    break;
+                }
+            }
+            return time;
+        }
+
+        private void OutofBridge(int weight) {
+            if (truck.Peek().time_stamp == weight) {
+                truck.Dequeue();
+            }
+        }
+
+        private void AddTimeStamp() {
+            foreach (var t in truck) {
+                t.time_stamp += 1;
+            }
+        }
+    }
+
+    class Truck
+    {
+        public int truck_weight = 0;
+        public int time_stamp = 0;
+
+        public Truck(int truck_weight, int time_stamp) {
+            this.truck_weight = truck_weight;
+            this.time_stamp = time_stamp;
+        }
+    }
+}
