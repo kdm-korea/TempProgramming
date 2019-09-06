feb4355aab72cfb46b66010cccfc3b8d37016531
KDM_korea <kdm_korea@naver.com>
2019-09-06 21:32:47 +0900

    Feat: Server programming start

---
+++ GameServer/GameServer/CListener.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System;
+namespace GameServer
+{
+    internal class CListener
+    {
+        SocketAsyncEventArgs accept_args;
+
+        Socket listen_socket;
+
+        AutoResetEvent flow_control_event;
+
+        public delegate void NewclientHandler(Socket client_socket, object token);
+        public NewclientHandler callback_on_newclient;
+
+        public CListener() {
+            this.callback_on_newclient = null;
+        }
+
+        public void start(string host, int port, int backlog) {
+            //creat sock
+            this.listen_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress address;
+
+            if(host == "0.0.0.0") {
+                address = IPAddress.Any;
+            }
+            else {
+                address = IPAddress.Parse(host);
+            }
+            IPEndPoint endpoint = new IPEndPoint(address, port);
+
+            try {
+                //socket binding host info and call listen method
+                this.listen_socket.Bind(endpoint);
+                this.listen_socket.Listen(backlog);
+
+                this.accept_args = new SocketAsyncEventArgs();
+                this.accept_args.Completed += new System.EventHandler<SocketAsyncEventArgs>(on_accept_completed);
+
+                this.listen_socket.AcceptAsync(this.accejjpt_args);
+            }
+            catch (System.Exception e) {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ GameServer/GameServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+
+namespace GameServer
+{
+    class CNetworkService
+    {
+        CListener client_listener;
+
+        SocketAsyncEventArgs recive_event_args_pool;
+        SocketAsyncEventArgs send_event_args_pool;
+
+        public delegate void SessionHandler(TcpClient tcpClient);
+        public SessionHandler session_created_callback { get; set; }
+    }
+}
