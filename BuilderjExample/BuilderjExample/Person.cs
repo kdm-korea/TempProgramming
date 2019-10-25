a99739b47febc92821e4c0940eed3c65d6dfd3b9
KDM_korea <kdm_korea@naver.com>
2019-10-25 22:11:37 +0900

    Feat: Builder pattern

---
+++ BuilderjExample/BuilderjExample/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderjExample
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        private string address;
+
+        public Person() {}
+
+        public Person Name(string name) {
+            this.name = name;
+            return this;
+        }
+
+        public Person Age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Person Address(string address) {
+            this.address = address;
+            return this;
+        }
+
+        public Person Build() {
+            return this;
+        }
+    }
+}
