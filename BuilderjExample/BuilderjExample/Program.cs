7681ec5a57967ae68426b7783a198c6c98cb6ce4
KDM_korea <kdm_korea@naver.com>
2019-10-25 22:11:24 +0900

    Feat: Project create

---
+++ BuilderjExample/BuilderjExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderjExample
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            List<Person> people = new List<Person>();
+
+            Person person = new Person()
+                .Address("aaa")
+                .Age(12)
+                .Name("fds")
+                .Build();
+
+            for (int idx = 0; idx < 30; idx++) {
+                people.Add(person);
+            }
+        }
+
+        public static void Example(params object[] str) {
+            for (int idx = 0; idx < str.Length; idx++) {
+                Console.WriteLine(str[idx]);
+            }
+        }
+    }
+}
